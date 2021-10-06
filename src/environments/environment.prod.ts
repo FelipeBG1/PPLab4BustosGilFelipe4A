be6008cb7bf2dd1224c847b29b24231988112463
FelipeBG1 <felibustosomg@gmail.com>
2021-10-06 19:19:13 -0300

    PPLab4

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB1YoLXfFCeQlVQMhOAdZuermLLVS_kkQQ",
+    authDomain: "pplab4-4b5ec.firebaseapp.com",
+    projectId: "pplab4-4b5ec",
+    storageBucket: "pplab4-4b5ec.appspot.com",
+    messagingSenderId: "434260557676",
+    appId: "1:434260557676:web:44cfb61d6e7d5a87df7d49"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB1YoLXfFCeQlVQMhOAdZuermLLVS_kkQQ",
+    authDomain: "pplab4-4b5ec.firebaseapp.com",
+    projectId: "pplab4-4b5ec",
+    storageBucket: "pplab4-4b5ec.appspot.com",
+    messagingSenderId: "434260557676",
+    appId: "1:434260557676:web:44cfb61d6e7d5a87df7d49"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
